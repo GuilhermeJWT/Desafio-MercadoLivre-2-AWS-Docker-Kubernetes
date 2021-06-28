bfd90f9fd52fbdb0544b9133c050721c81f9d098
GuilhermeJWT <guiromanno@gmail.com>
2021-06-28 10:49:43 -0300

    Finalizanda o Grafana e vendo tudo em Gráficos - Código 4701 JVM Micrometer

--- src/main/java/br/com/systemsgs/mercadolivre/MercadoLivreApplication.java
+++ src/main/java/br/com/systemsgs/mercadolivre/MercadoLivreApplication.java
@@ -1,12 +1,18 @@
 package br.com.systemsgs.mercadolivre;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableCaching
@@ -16,6 +22,12 @@ public class MercadoLivreApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+	    @Value("${spring.application.name}") String applicationName) {
+	    return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 	
 	@Override
